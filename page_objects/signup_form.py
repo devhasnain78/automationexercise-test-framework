5c7a0772b610bc9c0dd7b34133ee0c4c65e2e9b8
Hasnain Raza <experthasnain@gmail.com>
2026-03-11 06:13:56 +0500

    register user successfully

--- page_objects/signup_form.py
+++ page_objects/signup_form.py
@@ -3,13 +3,15 @@ class SignupForm:
         self.page = page
 
     def next_form(self, password, form_details):
-        self.page.get_by_role("radio", name="Mr").click()
+        self.page.get_by_role("radio", name="Mr.").click()
         self.page.locator("#password").fill(password)
         self.page.locator("#days").select_option("14")
         self.page.locator("#months").select_option("January")
         self.page.locator("#years").select_option("1997")
         self.page.get_by_role("checkbox", name="newsletter").check()
-        self.page.get_by_role("checkbox", name="optin").check()
+        self.page.locator("#optin").check()
 
         for field, value in form_details.items():
             self.page.locator(f"#{field}").fill(value)
+
+        self.page.get_by_role(role="button", name="Create Account").click()
