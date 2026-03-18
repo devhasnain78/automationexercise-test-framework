1681a71c8af4e5692660cffec290ab414c8e1df4
Hasnain Raza <experthasnain@gmail.com>
2026-03-18 06:01:38 +0500

    fix: add second product correctly to the cart

--- page_objects/add_product_page.py
+++ page_objects/add_product_page.py
@@ -1,14 +1,15 @@
 class AddProductPage:
     def __init__(self, page):
         self.page = page
+        self.selected_products = []
 
     def navigation(self):
         self.page.goto("https://automationexercise.com/", wait_until="domcontentloaded")
 
     def add_product(self):
-        self.selected_products = []
         nav = self.page.locator(".container")
         nav.get_by_role(role="link", name="Products").click()
+        modal = self.page.locator(".modal-content")
 
         for i in range(2):
             items = self.page.locator(".single-products")
@@ -20,15 +21,14 @@ class AddProductPage:
             btn = item.locator(".product-overlay .add-to-cart")
             btn.wait_for(state="visible")
             btn.click()
-            modal = self.page.locator(".modal-content")
+            modal.wait_for(state="visible")
             if i==0:
                 modal.get_by_role(role="button", name="Continue Shopping").click()
-            else:
-                modal.get_by_role(role="link", name="View Cart").click()
-            return self.selected_products
+        modal.get_by_role(role="link", name="View Cart").click()
+        return self.selected_products
 
     def view_cart(self):
-        self.page.goto("https://automationexercise.com/view_cart")
+        # self.page.goto("https://automationexercise.com/view_cart")
         cart = self.page.locator(".cart_price p").all_text_contents()
         return cart
 
